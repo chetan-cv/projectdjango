5c5501ba28303a500d99b40bcdf526203213a89c
chetan-cv <chetanverma99.cv@gmail.com>
2019-08-14 23:39:45 +0530

    added models

--- Desktop/projectdjango/homepage/models.py
+++ Desktop/projectdjango/homepage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Hompeage(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    photo1 = models.ImageField(upload_to='photos', blank=True)
+    photo2 = models.ImageField(upload_to='photos', blank=True)
+    photo3 = models.ImageField(upload_to='photos', blank=True)
+    photo4 = models.ImageField(upload_to='photos', blank=True)
+    photo5 = models.ImageField(upload_to='photos', blank=True)
+    photo6 = models.ImageField(upload_to='photos', blank=True)
+
+    def __str__(self):
+        return self.title
